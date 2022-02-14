7fb191065ac56215e51baa608d81f7d6d503a02e
kyle <kyle.aoki.500@gmail.com>
2022-02-13 20:46:13 -0800

    a

---
+++ note_api/target/debug/deps/note_api-918ee65ddabafa08.d
@@ -0,0 +1,5 @@
+/Users/kyle/github/note/note_api/target/debug/deps/note_api-918ee65ddabafa08.rmeta: src/main.rs
+
+/Users/kyle/github/note/note_api/target/debug/deps/note_api-918ee65ddabafa08.d: src/main.rs
+
+src/main.rs:
---
+++ note_api/target/debug/deps/note_api-e949eb65bf378a74.d
@@ -0,0 +1,5 @@
+/Users/kyle/github/note/note_api/target/debug/deps/note_api-e949eb65bf378a74.rmeta: src/main.rs
+
+/Users/kyle/github/note/note_api/target/debug/deps/note_api-e949eb65bf378a74.d: src/main.rs
+
+src/main.rs:
